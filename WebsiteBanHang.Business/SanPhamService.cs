dfa2d1ca179ec39222b7ba78a774befae7d7e48b
giaphong2004 <2200010465@nttu.edu.vn>
2025-10-11 02:49:29 +0700

    Chuẩn hóa cấu trúc thư mục theo mô hình 3 lớp tt

---
+++ WebsiteBanHang.Business/SanPhamService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebsiteBanHang.Data;
+
+namespace WebsiteBanHang.Business
+{
+    public class SanPhamService
+    {
+        private CSDL_PetShopEntities _db = new CSDL_PetShopEntities();
+
+        // Phương thức để lấy về TẤT CẢ sản phẩm
+        public List<SanPham> LayTatCaSanPham()
+        {
+            // Truy cập vào bảng SanPhams trong DB, và chuyển nó thành một danh sách (List)
+            return _db.SanPhams.ToList();
+        }
+    }
+}
