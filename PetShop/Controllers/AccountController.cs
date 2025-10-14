775407755a002872c77746a6888dd3531dc7a2cc
giaphong2004 <2200010465@nttu.edu.vn>
2025-10-14 15:11:53 +0700

    link các button ở header và 2 tính năng login signup,băm mk bằng thư viện Bcrypt

---
+++ PetShop/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebsiteBanHang.Business;
+using WebsiteBanHang.Data;
+using WebsiteBanHang.Models;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserService _userService = new UserService();
+        // GET: Account
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(string username ,string password,string fullName) 
+        {
+            if (_userService.IsUserNameExists(username))
+            {
+                ModelState.AddModelError("","Tên User này đã được sử dụng");
+                return View();
+            }
+
+            User newUser = _userService.RegisterUser(username, password, fullName);
+
+            return RedirectToAction("Login");
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username,string password,string returnUrl)
+        {
+            User user = _userService.Login(username, password);
+            if(user != null)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                Session["FullName"] = user.TenNguoiDung;
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không chính xác");
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+            
+    }
+}
---
+++ WebsiteBanHang.Business/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebsiteBanHang.Data;
+
+
+namespace WebsiteBanHang.Business
+{
+    using BCrypt.Net;
+    public class UserService
+    {
+        private CSDL_PetShopEntities _db = new CSDL_PetShopEntities();
+        public bool IsUserNameExists(string userName)
+        {
+            return _db.Users.Any(u => u.UserName == userName);
+        }
+
+        public User RegisterUser(string userName,string password,string fullName)
+        {
+            string hashedPassWork = BCrypt.HashPassword(password);
+
+            var newUser = new User()
+            {
+                UserName = userName,
+                PasswordHash = hashedPassWork,
+                TenNguoiDung = fullName
+            };
+
+            _db.Users.Add(newUser);
+            _db.SaveChanges();
+
+            return newUser;
+        }
+
+        public User Login(string userName, string password) 
+        {
+            var user = _db.Users.FirstOrDefault(u => u.UserName == userName);
+            if (user != null)
+            {
+                if (BCrypt.Verify(password, user.PasswordHash)) 
+                { 
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+}
